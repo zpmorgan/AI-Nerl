402ba96fc39beac6f39c95d14abc2a31f4db9ca3
Zach Morgan <zpmorgan@gmail.com>
2012-01-10 04:49:45 -0500

    blah. about to try using tanh

--- lib/AI/Nerl/Network.pm
+++ lib/AI/Nerl/Network.pm
@@ -4,6 +4,8 @@ use PDL;
 use PDL::NiceSlice;
 use PDL::Constants 'E';
 
+my $DEBUG=0;
+
 # http://ufldl.stanford.edu/wiki/index.php/Backpropagation_Algorithm
 # http://www.stanford.edu/class/cs294a/sparseAutoencoder_2011new.pdf
 
@@ -35,6 +37,19 @@ has theta2 => (
    lazy => 1,
    builder => '_mk_theta2',
 );
+
+has b1 => (
+   is => 'ro',
+   isa => 'PDL',
+   lazy => 1,
+   builder => '_mk_b1',
+);
+has b2 => (
+   is => 'ro',
+   isa => 'PDL',
+   lazy => 1,
+   builder => '_mk_b2',
+);
 has alpha => ( #learning rate
    isa => 'Num',
    is => 'rw',
@@ -43,16 +58,24 @@ has alpha => ( #learning rate
 has lambda => (
    isa => 'Num',
    is => 'rw',
-   default => .01,
+   default => .0,
 );
 
 sub _mk_theta1{
    my $self = shift;
-   return grandom($self->l1, $self->l2) * .1;
+   return grandom($self->l1, $self->l2) * .01;
 }
 sub _mk_theta2{
    my $self = shift;
-   return grandom($self->l2, $self->l3) * .1;
+   return grandom($self->l2, $self->l3) * .01;
+}
+sub _mk_b1{
+   my $self = shift;
+   return grandom($self->l2) * .01;
+}
+sub _mk_b2{
+   my $self = shift;
+   return grandom($self->l3) * .01;
 }
 
 
@@ -67,35 +90,49 @@ sub train{
 
    for my $pass (1..$passes){
 #      warn 'blah:'. $self->theta1->slice(':,2')->flat->sum;
-#      show784($self->theta1->slice(':,2'));
+      show784($self->theta1->slice(':,2'));
       my $delta1 = $self->theta1->copy * 0;
       my $delta2 = $self->theta2->copy * 0;
+      my $deltab1 = $self->b1->copy * 0;
+      my $deltab2 = $self->b2->copy * 0;
+
       #iterate over examples :(
       for my $i (0..$num_examples-1){
          my $a1 = $x(($i));
          my $z2 = ($self->theta1 x $a1->transpose)->squeeze;
+         $z2 += $self->b1; #add bias.
          my $a2 = sigmoid($z2);
          my $z3 = ($self->theta2 x $a2->transpose)->squeeze;
+         $z3 += $self->b2; #add bias.
          my $a3 = sigmoid($z3);
          # warn $y(($i)) - $a3;
          my $d3= -($y(($i)) - $a3) * logistic($a3);
+         #warn $d3;
          $delta2 += $d3->transpose x $a2;
          my $d2 = ($self->theta2->transpose x $d3->transpose)->squeeze * logistic($a2);
          $delta1 += $d2->transpose x $a1;
          #warn $delta2(4);
-         if(0 and rand()<.01){
-            warn (($d3->transpose x $a2)->flat->sum);;
-            warn ($z3);
-            warn $d2->sum;
-            warn (($d2->transpose x $a1)->flat->sum);;
+         $deltab1 += $d2;
+         $deltab2 += $d3;
+
+         if($DEBUG==1){
+            warn "z2: $z2\n$z3: $z3\n";
+            warn "d3:$d3\n";
          }
       }
-      warn $delta2;#/ $num_examples;
-      warn $delta1 ;
+      #warn $deltab1;
+      if($DEBUG==1){
+         warn "theta1: ". $self->theta1;#/ $num_examples;
+         warn "theta2: ". $self->theta2;
+         warn "delta1: $delta1\n";
+         warn "delta2: $delta2\n";
+      }
       $self->{theta2} -= $self->alpha * ($delta2 / $num_examples + $self->theta2 * $self->lambda);
       $self->{theta1} -= $self->alpha * ($delta1 / $num_examples + $self->theta1 * $self->lambda);
+      $self->{b1} -= $self->alpha * $deltab1 / $num_examples;
+      $self->{b2} -= $self->alpha * $deltab2 / $num_examples;
 #      warn "theta1 wt total: ".$self->theta1->flat->sum;
-      my ($cost,$numcorrect) = $self->cost($x,$y);
+#      my ($cost,$numcorrect) = $self->cost($x,$y);
 #      warn "cost: $cost. \nclassified correctly: $numcorrect";
       
    }
@@ -104,8 +141,13 @@ sub train{
 sub run{
    my ($self,$x) = @_;
    my $y = $self->theta1 x $x;
+   #warn $y;
+   #warn $self->b1;
+   $y += (ones(1,$x->dim(0)) x $self->b1)->transpose;
    $y = sigmoid($y);
    $y = $self->theta2 x $y;
+   $y += $self->b2;
+   warn $y;
    $y = sigmoid($y);
    return $y;
 }
--- t/nerl_network.t
+++ t/nerl_network.t
@@ -8,6 +8,32 @@ use_ok('AI::Nerl::Network');
    isa_ok($nn,'AI::Nerl::Network');
 }
 
+#simplest 3layer: 1 in, i hidden, 1 out
+#1->1,0->0
+{
+   my $x = pdl(0,1);
+   my $y = pdl(1,0);
+   my $nn = AI::Nerl::Network->new(
+      l1=>1,l2=>1,l3=>1,
+      lambda=>0,
+      theta1=>pdl(6),
+      theta2=>pdl(6),
+      b1=>pdl(0),
+      b2=>pdl(0),
+      alpha=>100,
+   );
+   for(1..99){
+      die $nn->run($x);
+   diag($nn->run($x));
+      $nn->train($x,$y,passes=>3);
+      my $b1 = $nn->b2;
+   diag($nn->run($x));
+      die $b1 . 'foo';
+      $b1->slice(0) .= 0;
+   }
+   diag($nn->run($x));
+}
+
 #build a few really simple nets.
 #in: x1,x2
 #out: x1 AND|OR|XOR x2
@@ -20,18 +46,23 @@ use_ok('AI::Nerl::Network');
    my ($AND_nn,$OR_nn,$XOR_nn) = map {
       AI::Nerl::Network->new(
          l1=>2,
-         l2=>3,
+         l2=>2,
          l3=>1,
+         alpha=>1000,
+         lambda=>0,
       );
    } 1..3;
    isa_ok($XOR_nn, 'AI::Nerl::Network');
    is($XOR_nn->theta1->dim(0), 2, 'theta1 dim1 == 2');
-   is($XOR_nn->theta1->dim(1), 3, 'theta1 dim2 == 3');
-   is($XOR_nn->theta2->dim(0), 3, 'theta1 dim1 == 3');
-   is($XOR_nn->theta2->dim(1), 1, 'theta1 dim2 == 1');
-
-   $AND_nn->train($x,$AND, passes=>311);
+   is($XOR_nn->theta1->dim(1), 2, 'theta1 dim2 == 2');
+   is($XOR_nn->theta2->dim(0), 2, 'theta2 dim1 == 2');
+   is($XOR_nn->theta2->dim(1), 1, 'theta2 dim2 == 1');
+   my @Js;
+   for (1..10){
+      $AND_nn->train($x,$XOR, passes=>99);
+   }
+   warn $XOR_nn->b1;
    my $AND_output = $AND_nn->run($x);
-   diag($AND_output);
+   warn("XOR: ".$AND_output);
 
 }
